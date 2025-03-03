cf5c6516249866621a91c7f15c1ca867e8e7e5ce
BassaniDavid <davbassan98@gmail.com>
2025-03-03 16:53:55 +0100

    feat: snack5 complete with arrow function

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -9,10 +9,57 @@ const name = 'Mario';
 
 
 // Dichiara la funzione qui.
+function hello (userName) {
+    
+    // controllo se la stringa inserita non ha numeri o spazi
+    for (let i = 0; i < userName.length; i++) {
+        
+        if(!isNaN(userName[i]) || userName[i] === " " ){
+            console.log(false) 
+            return 'Inserisci un nome valido'
+        }
+    }
 
+    const d = new Date()
+    if(d.getHours()< 13){
+        return `buongiorno ${userName}`
+    }
+    else if(d.getHours()>= 13 && d.getHours()< 17){
+        return `buon pomeriggio ${userName}`
+    }
+    else if(d.getHours()>= 17)
+    return `buonasera ${userName}`
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(hello(name))
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+
+// arrow function
+let hello2 = (userName) => {
+    
+    // controllo se la stringa inserita non ha numeri o spazi
+    for (let i = 0; i < userName.length; i++) {
+        
+        if(!isNaN(userName[i]) || userName[i] === " " ){
+            console.log(false) 
+            return 'Inserisci un nome valido'
+        }
+    }
+
+    const d = new Date()
+    if(d.getHours()< 13){
+        return `buongiorno ${userName}`
+    }
+    else if(d.getHours()>= 13 && d.getHours()< 17){
+        return `buon pomeriggio ${userName}`
+    }
+    else if(d.getHours()>= 17)
+    return `buonasera ${userName}`
+}
+
+console.log(hello2(name))
