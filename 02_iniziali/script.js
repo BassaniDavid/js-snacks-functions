9ace9fc987a2e87eb1b300c992f609d799192aa2
BassaniDavid <davbassan98@gmail.com>
2025-03-03 15:41:16 +0100

    feat: insert all the names in the new array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,42 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function funzione (input) {
 
+    // controllo array che non ci siano numeri
+    for (let i = 0; i < input.length; i++) {
+        if(isNaN(input[i]) === false) {
+            return 'array non valido'
+        } 
+        else{
+
+            // nuovo array in cui inserire le iniziali
+            let inArray = []
+
+            // selezionare le iniziali del primo array e inserirle nel secondo array
+            for (let i = 0; i < input.length; i++) {
+                inArray.push(input[i])
+                
+                
+            }
+            return inArray
+        }
+    }
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+funzione(names)
+console.log(funzione(names))
+
 
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+
+
+
+
