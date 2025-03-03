e161632610aabdc14c62e51bb0716a6a34fe63ba
BassaniDavid <davbassan98@gmail.com>
2025-03-03 15:48:38 +0100

    feat: insert only the first letters in the second array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -11,25 +11,27 @@ function funzione (input) {
         if(isNaN(input[i]) === false) {
             return 'array non valido'
         } 
-        else{
-
-            // nuovo array in cui inserire le iniziali
-            let inArray = []
-
-            // selezionare le iniziali del primo array e inserirle nel secondo array
-            for (let i = 0; i < input.length; i++) {
-                inArray.push(input[i])
-                
-                
-            }
-            return inArray
-        }
     }
+    // nuovo array in cui inserire le iniziali
+    let inArray = []
+
+    // selezionare le iniziali del primo array e inserirle nel secondo array
+    for (let i = 0; i < input.length; i++) {
+    
+        // trasforma l'elemento selezionato dell' array in una stringa
+        let stringa =input[i].toString()
+    
+        // inserisci la prima lettera della stringa nel secondo array
+        inArray.push(stringa[0])
+                    
+    }
+    return inArray
 
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 funzione(names)
+
 console.log(funzione(names))
 
 
