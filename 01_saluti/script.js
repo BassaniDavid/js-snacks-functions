8f24ff6695b3a80881b2360d79c839518d47cd6c
BassaniDavid <davbassan98@gmail.com>
2025-03-03 14:54:35 +0100

    feat: control if there are any numbers or spaces in the input

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,25 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function hello (name) {
+    
+    // controllo se la stringa inserita non ha numeri o spazi
+    for(let i = 0; i < name.length; i++){
+        
+        if(!isNaN(name[i]) || name[i] === " " ){
+            return false
+        }
+        else {
+            return true
+        }
+    }
+        
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+hello(userName)
+console.log(hello(userName))
+let result=hello(userName)
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
