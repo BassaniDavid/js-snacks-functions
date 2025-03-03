140cb6f30cae046bf684b1bf6b8225b1fb1466e1
BassaniDavid <davbassan98@gmail.com>
2025-03-03 16:14:39 +0100

    feat: string control

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,13 +1,25 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
+const string ='stringadicontrollo'
 
 
 // Dichiara la funzione qui.
 
+function funzione (input) {
+
+    // controllo che la variabile sia una stringa
+    if (typeof word !== typeof string) {
+        return 'non è una stringa'
+    }
+
+    // controllo ogni lettera e se è uguale ad una vocale aumento di 1 una variabile
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(funzione (word))
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
